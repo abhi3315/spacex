ff5d6d37631a16891cdddf01fa47337e0e41a10b
Abhishek Sharma <abhishek3.dev@gmail.com>
2023-03-02 14:12:45 +0530

    feat: setup eslint, prettier, commitlint and husky

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react'],
+	rules: {
+		'no-console': 'warn',
+		'no-unused-vars': 'warn',
+		'react/react-in-jsx-scope': 'off',
+		'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+	},
+};
