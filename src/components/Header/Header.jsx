996ce692761c5201c241a11fe4528cff7dfd5066
Abhishek Sharma <abhishek3.dev@gmail.com>
2023-03-11 14:46:01 +0530

    feat: header & hero-banner component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,69 @@
+import PropTypes from 'prop-types';
+
+import DarkShuttleIcon from '../../assets/shuttle-dark.png';
+import LightShuttleIcon from '../../assets/shuttle-light.png';
+
+const DarkIcon = (
+	<svg
+		xmlns='http://www.w3.org/2000/svg'
+		viewBox='0 0 20 20'
+		fill='currentColor'
+		aria-hidden='true'
+		className='w-5 h-5'
+	>
+		<path d='M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z' />
+	</svg>
+);
+
+const LightIcon = (
+	<svg
+		xmlns='http://www.w3.org/2000/svg'
+		viewBox='0 0 20 20'
+		fill='currentColor'
+		aria-hidden='true'
+		className='w-5 h-5'
+	>
+		<path
+			fillRule='evenodd'
+			d='M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z'
+			clipRule='evenodd'
+		/>
+	</svg>
+);
+
+function Header({ toggleTheme, theme }) {
+	return (
+		<header>
+			<nav className='bg-white border-gray-200 px-10 xl:px-32 shadow-2xl mb-10 py-2.5 dark:bg-zinc-800'>
+				<div className='flex flex-wrap justify-between items-center mx-auto max-w-screen-xl'>
+					<div href='/' className='flex items-center'>
+						<img
+							src={theme === 'dark' ? LightShuttleIcon : DarkShuttleIcon}
+							className='mr-3 h-6 sm:h-9'
+							alt='Light Space Shuttle Logo'
+						/>
+						<span className='self-center text-xl font-semibold whitespace-nowrap dark:text-white'>
+							Shuttle
+						</span>
+					</div>
+					<div className='flex items-center lg:order-2'>
+						<button
+							onClick={toggleTheme}
+							type='button'
+							className='text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 inline-flex items-center'
+						>
+							{theme === 'dark' ? LightIcon : DarkIcon}
+						</button>
+					</div>
+				</div>
+			</nav>
+		</header>
+	);
+}
+
+Header.propTypes = {
+	toggleTheme: PropTypes.func.isRequired,
+	theme: PropTypes.string.isRequired,
+};
+
+export default Header;
---
+++ src/components/HeroBanner/HeroBanner.jsx
@@ -0,0 +1,22 @@
+import RocketImage from '../../assets/rocket.webp';
+
+function HeroBanner() {
+	return (
+		<div className='grid max-w-screen-xl px-4 md:px-10 py-4 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12'>
+			<div className='mr-auto place-self-center lg:col-span-7'>
+				<h1 className='max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white'>
+					SpaceX Shuttle Diary
+				</h1>
+				<p className='max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400'>
+					Stay updated with SpaceX rocket information for the latest news and updates on their
+					innovative technology and space exploration missions.
+				</p>
+			</div>
+			<div className='hidden lg:mt-0 lg:col-span-5 lg:flex'>
+				<img src={RocketImage} alt='Rocket' />
+			</div>
+		</div>
+	);
+}
+
+export default HeroBanner;
